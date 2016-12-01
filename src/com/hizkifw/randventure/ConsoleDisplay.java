c090334038d4e936fa22cedc1642aabc6a8549aa
HizkiFW <hizkifw@gmail.com>
2016-12-01 21:53:07 +0700

    Added clear console function

--- src/com/hizkifw/randventure/ConsoleDisplay.java
+++ src/com/hizkifw/randventure/ConsoleDisplay.java
@@ -1,16 +1,32 @@
 package com.hizkifw.randventure;
 
+import java.io.IOException;
+
 public class ConsoleDisplay {
 	public static final int WIDTH = 80;
 	
 	public static String centerText(String text) {
-		return String.format("%1$" + Math.floor(WIDTH/2 - text.length()/2) + "s", text);
+		return generateSequence(" ", (int) Math.floor(WIDTH/2 - text.length()/2)) +  text;
 	}
-	public static String generateLine(String chr) {
-		return String.format("%1$" + WIDTH + "s", chr).replace(" ", chr);
+	public static String generateSequence(String chr, int n) {
+		String retval = "";
+		for(int i = 0; i < n; i++) {
+			retval += chr;
+		}
+		return retval;
+	}
+	
+	public static void clearConsole() {
+		try {
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+		} catch(InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 	public static void printOut() {
-		System.out.println(generateLine("-"));
+		clearConsole();
+		System.out.println(generateSequence("-", WIDTH));
 		System.out.println(centerText("Random Adventure"));
+		System.out.println(generateSequence("-", WIDTH));
 	}
 }
