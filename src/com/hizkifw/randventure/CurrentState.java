3388f7fc28a409e893c34c094b701a86aa582ecf
HizkiFW <hizkifw@gmail.com>
2016-12-02 11:12:02 +0700

    Added class for storing current progress

---
+++ src/com/hizkifw/randventure/CurrentState.java
@@ -0,0 +1,9 @@
+package com.hizkifw.randventure;
+
+import com.hizkifw.randventure.game.*;
+
+public class CurrentState {
+	public static Player player;
+	public static Map map;
+	public static Place currentPlace;
+}
