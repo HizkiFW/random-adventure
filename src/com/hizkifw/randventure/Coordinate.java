ea2327f9aa889ab75f7bf951cb4e1edbb23749d2
HizkiFW <hizkifw@gmail.com>
2016-12-02 11:10:31 +0700

    Coordinate class

---
+++ src/com/hizkifw/randventure/Coordinate.java
@@ -0,0 +1,10 @@
+package com.hizkifw.randventure;
+
+public class Coordinate {
+	public final double x;
+	public final double y;
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
