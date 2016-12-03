4b9883a29f2f335b29ce84b61b333338f3ddf858
HizkiFW <hizkifw@gmail.com>
2016-12-03 10:57:48 +0700

    Added narration templates

---
+++ src/com/hizkifw/randventure/NarrationTemplate.java
@@ -0,0 +1,8 @@
+package com.hizkifw.randventure;
+
+public class NarrationTemplate {
+	public static String[] fillers = {
+		"You find yourself in $place. You can see paths to the $directions. Where do you go?",
+		"You see $rdir_place in the $rdir."
+	};
+}
